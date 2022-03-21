af155e2cf21afd52bec38a78993185328c640c96
AlexWilliam <alex.william@outlook.com>
2022-03-21 17:48:42 -0300

    Adicionando estrutura de camadas
    Adicionando camadas de controller, model, service e converterextension para a estrutura do projeto

---
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -0,0 +1,42 @@
+package com.mercadolivro.service
+
+import com.mercadolivro.model.CustomerModel
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService {
+
+    val customers = mutableListOf<CustomerModel>()
+
+    fun getAll(name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
+        return customers
+    }
+
+    fun create(customer: CustomerModel) {
+
+        customer.id = if(customers.isEmpty()) {
+            1
+        }else{
+            customers.last().id!! + 1
+        }
+        customers.add(customer)
+    }
+
+    fun getCustomer(id: Int): CustomerModel {
+        return customers.first { it.id == id }
+    }
+
+    fun update(customer: CustomerModel){
+        customers.first { it.id == customer.id }.let{
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    fun delete(id: Int){
+        customers.removeIf{ it.id == id }
+    }
+}
